c24cbe775a86b4d2d3f72fb2a5980b20f01d63f7
amaramdotme <amaramdotme@gmail.com>
2025-12-20 18:44:56 -0500

    fix: restore missing networking variables in workloads module

--- modules/workloads/variables.tf
+++ modules/workloads/variables.tf
@@ -14,6 +14,49 @@ variable "common_tags" {
 }
 
 variable "log_analytics_workspace_id" {
+
   description = "Resource ID of the central Log Analytics Workspace for diagnostics"
+
   type        = string
+
+}
+
+
+
+variable "vnet_address_space" {
+
+  description = "Address space for the Workload VNet"
+
+  type        = list(string)
+
+}
+
+
+
+variable "subnet_aks_prefix" {
+
+  description = "Address prefixes for the AKS nodes subnet"
+
+  type        = list(string)
+
+}
+
+
+
+variable "subnet_ingress_prefix" {
+
+  description = "Address prefixes for the Ingress subnet"
+
+  type        = list(string)
+
+}
+
+
+
+variable "acr_id" {
+
+  description = "Resource ID of the global ACR for AcrPull assignment"
+
+  type        = string
+
 }
